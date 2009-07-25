c1c928e3b18b80f2e29e983b52492bccc0eebf52
Roland Swingler <roland.swingler@gmail.com>
2009-07-25 18:39:54 +0100

    Significantly improving rails template

--- boots.rb
+++ boots.rb
@@ -1,9 +1,34 @@
-gem :authlogic
+APP_NAME = @root.split('/').last
+# Is rails app part of a larger, pre-gitified project or not?
+# For some reason git status returns 256 instead of 0 when successful on my system
+SUBPROJECT = system("git status > /dev/null 2> /dev/null") || $? == 256
+
+git :init unless SUBPROJECT
+
+# Git ignore files
+file "log/.gitignore", <<-FILE
+*.log
+*.pid
+FILE
+
+file ".gitignore", <<-FILE
+config/database.yml
+FILE
+
+file "tmp/.gitignore", <<-FILE
+**/*
+FILE
+
+git :add => "."
+git :commit => "-m 'Initial rails skeleton commit'"
+
+# Gems
+gem "authlogic-oid", :lib => "authlogic_openid"
 gem "ruby-openid", :lib => "openid"
-gem "authlogic-oid"
+gem "authlogic"
+gem "configatron"
+gem "date-performance", :lib => "date/performance"
 gem "mislav-will_paginate", :lib => "will_paginate", :source => "http://gems.github.com"
-plugin :open_id_authentication, :git => "git://github.com/rails/open_id_authentication.git"
-plugin :hoptoad_notifier, :git => "git://github.com/thoughtbot/hoptoad_notifier.git"
 
 # Testing tools
 gem :faker, :env => "test"
@@ -12,16 +37,86 @@ gem :fakeweb, :env => "test"
 gem :mocha, :env => "test"
 gem :webrat, :env => "test"
 gem :cucumber, :env => "test"
-plugin :no_peeping_toms, :git => "git://github.com/pat-maddox/no-peeping-toms.git"
 
-generate :cucumber
+# Plugins
+plugin :open_id_authentication, 
+  :git => "git://github.com/rails/open_id_authentication.git", :submodule => true
+plugin :hoptoad_notifier, 
+  :git => "git://github.com/thoughtbot/hoptoad_notifier.git", :submodule => true
+plugin :rails_sql_views, 
+  :git => "git://github.com/aeden/rails_sql_views.git", :submodule => true
+plugin :no_peeping_toms, 
+  :git => "git://github.com/pat-maddox/no-peeping-toms.git", :submodule => true
+
+# Not using rake command because I want gems to be installed globally, not in ~/.gem
+rake "gems:install", :sudo => true
 
 # Clear out some irrelevant files
 run "rm public/index.html"
 run "rm README"
+run "rm public/favicon.ico"
 run "rm public/images/rails.png"
 run "rm public/javascripts/*.js"
+run "rm config/database.yml"
+run "cp /usr/local/share/jquery/jquery-1.3.2.js public/javascripts/jquery-1.3.2.js"
 
-run "cp config/database.yml config/database.example.yml"
+# Database.yml
+file "config/database.yml.example", <<-FILE
+development: &default_settings 
+  adapter: mysql
+  encoding: utf8
+  reconnect: true
+  pool: 5
+  socket: /var/run/mysqld/mysqld.sock
+  database: #{APP_NAME}_development
+  username: root
+  password:
 
-# TODO add config.active_record.timestamped_migrations = false to environment.rb
+test:
+  <<: *default_settings
+  database: #{APP_NAME}_test
+
+production:
+  <<: *default_settings
+  database: #{APP_NAME}
+  username: #{APP_NAME}
+  password:
+FILE
+run "cp config/database.yml.example config/database.yml"
+
+# Deployment configuration
+file "config/deploy.rb", <<-FILE
+set :application, "#{APP_NAME}"
+set :domain, "192.168.0.10"
+set :deploy_to, "/usr/local/share/sites/\#{application}"
+set :repository, "ssh://192.168.0.4#{@root}"
+set :revision, "head"
+FILE
+
+rakefile("vlad.rake") do
+<<-FILE
+begin
+  require 'vlad'
+  require 'vlad/core'
+  require 'vlad/passenger'
+  require 'vlad/git'
+
+  Kernel.load 'config/deploy.rb'
+rescue LoadError
+  # Do nothing
+end
+FILE
+end
+
+env_file = File.read("config/environment.rb")
+env_file.sub!(/^end$/, "  config.active_record.timestamped_migrations = false\nend")
+file "config/environment.rb", env_file
+
+generate :cucumber
+generate :session, "UserSession"
+
+rake "db:create"
+rake "db:create RAILS_ENV=test"
+
+git :add => "."
+git :commit => "-a -m 'Additions from boots rails template'"
